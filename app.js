635cb9fffa37e77c2685c3e6c966af204af0569c
Anders Halvarsson <anders.halvarsson@gmail.com>
2024-06-01 12:20:56 +0200

    ``` Enhance code quality and improve logging functionality
    - Updated .prettierrc tabWidth from 3 to 2 for better readability.
    - Standardized formatting in app.js for consistency.
    - Switched from eslint.config.mjs to eslint.config.js.
    - Augmented helper.js to support additional task attributes.
    - Enhanced logging functionality in logger.js:
      - Added email notifications using nodemailer.
      - Consolidated log structure and targets for different log levels.
    - Applied error categorization in queueManager.js.
    - Added nodemailer dependency in package.json.
    
    These changes improve code formatting consistency and error tracking mechanisms, thereby enhancing overall code quality and maintainability.
    ```

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var jwt  = require( 'jsonwebtoken' );
-var logging = require( './lib/logger.js' );
+var jwt = require('jsonwebtoken');
+var logging = require('./lib/logger.js');
 
 var indexRouter = require('./routes/index');
-var pdfRouter = require( './routes/pdf' );
-var loginRouter = require( './routes/login' );
-var tokenRouter = require( './routes/token' );
+var pdfRouter = require('./routes/pdf');
+var loginRouter = require('./routes/login');
+var tokenRouter = require('./routes/token');
 
 var app = express();
 
@@ -17,48 +17,46 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 // Add this middleware function to log all incoming requests
 app.use((req, res, next) => {
-  console.log(`Received ${req.method} request to ${req.path}`);
-  next();
+   console.log(`Received ${req.method} request to ${req.path}`);
+   next();
 });
 
 // routes are here
 app.use('/', indexRouter);
 app.get('/testServerOnline', (req, res) => {
-  res.send('Server is running');});
-app.use( '/login', loginRouter );
-app.use( '/token', tokenRouter );
+   res.send('Server is running');
+});
+app.use('/login', loginRouter);
+app.use('/token', tokenRouter);
 app.use('/pdf', verifyToken, pdfRouter);
 app.get('/pdfs/:filename', (req, res) => {
-  res.sendFile(path.join(__dirname, 'routes', 'tmp') + '/' + req.params.filename);
+   res.sendFile(path.join(__dirname, 'lib', 'tmp') + '/' + req.params.filename);
 });
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next( createError( 404 ) );
+app.use(function (req, res, next) {
+   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+   // set locals, only providing error in development
+   res.locals.message = err.message;
+   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+   // render the error page
+   res.status(err.status || 500);
+   res.render('error');
 });
-
 
 /**
  * Middleware function to verify the access token in the request headers
@@ -67,30 +65,32 @@ app.use(function(err, req, res, next) {
  * @param {Function} next - The next middleware function
  */
 function verifyToken(req, res, next) {
-  try {
-    const authHeader = req.headers.authorization;
-    if (authHeader) {
-      const token = authHeader.split(' ')[1];
-      jwt.verify(token, process.env.SECRET, (err, user) => {
-        if (err) {
-          return res.sendStatus(403); // Forbidden
-        }
+   try {
+      const authHeader = req.headers.authorization;
+      if (authHeader) {
+         const token = authHeader.split(' ')[1];
+         jwt.verify(token, process.env.SECRET, (err, user) => {
+            if (err) {
+               return res.sendStatus(403); // Forbidden
+            }
 
-        req.user = user;
-        next();
+            req.user = user;
+            next();
+         });
+      } else {
+         res.sendStatus(401); // Unauthorized
+      }
+   } catch (error) {
+      logging.error({
+         message: 'verifyToken error',
+         metadata: error,
+         codeFile: 'app.js',
       });
-    } else {
-      res.sendStatus(401); // Unauthorized
-    }
-  } catch (error) {
-    logging.error({
-      message: 'verifyToken error',
-      metadata: error,
-      codeFile: 'app.js',
-    });
-    if (!res.headersSent) res.status(500).send('An error occurred while trying to verify token.');
-  }
+      if (!res.headersSent)
+         res.status(500).send(
+            'An error occurred while trying to verify token.'
+         );
+   }
 }
-
 
 module.exports = app;
---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+const globals = require('globals');
+const pluginJs = require('@eslint/js');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const pkg = require('eslint-config-prettier');
+const { rules } = pkg;
+
+module.exports = [
+   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+   pluginJs.configs.recommended,
+
+   eslintPluginPrettierRecommended,
+
+   {
+      rules: {
+         ...rules,
+         semi: 'error',
+         quotes: ['error', 'single'],
+         'space-before-blocks': 'error',
+         'space-in-parens': 'error',
+         'space-infix-ops': 'error',
+      },
+   },
+];
